8223e4dfa13a7e138158b1735e75a936da35b5ce
ViktoriiaB <93058921+ViktoriiaB@users.noreply.github.com>
2021-12-11 23:22:08 +0200

    Update DemoQATest.java

--- ninthLesson/src/test/java/DemoQATest.java
+++ ninthLesson/src/test/java/DemoQATest.java
@@ -1,6 +1,7 @@
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -31,17 +32,28 @@ public class DemoQATest {
     @Test
     public void firstTest() {
         driver.get(URL);
+
+        WebElement ads = driver.findElement(By.id("adplus-anchor"));
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("arguments[0].parentNode.removeChild(arguments[0])", ads);
+
         driver.findElement(By.xpath("//img[@src='/images/WB.svg']")).click();
+        List<String> newTab = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(newTab.get(newTab.size() - 1));
 
         Assert.assertEquals(driver.getCurrentUrl(), URL2);
 
-        List<String> tabs = new ArrayList<>(driver.getWindowHandles());
-        System.out.println(tabs.size());
+        System.out.println(newTab.size());
     }
 
     @Test
     public void secondTest() throws InterruptedException {
         driver.get(URL);
+
+        WebElement ads = driver.findElement(By.id("adplus-anchor"));
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("arguments[0].parentNode.removeChild(arguments[0])", ads);
+
         Thread.sleep(3000);
 
         WebElement cardElements = driver.findElement(By.xpath("//*[contains(@class, 'top-card')]//*[contains(text(), 'Elements')]"));
@@ -68,6 +80,10 @@ public class DemoQATest {
     @Test
     public void thirdTest() throws InterruptedException {
         driver.get(URL);
+        WebElement ads = driver.findElement(By.id("adplus-anchor"));
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("arguments[0].parentNode.removeChild(arguments[0])", ads);
+
         Thread.sleep(3000);
         WebElement cardElements = driver.findElement(By.xpath("//*[contains(@class, 'top-card')]//*[contains(text(), 'Elements')]"));
         cardElements.click();
@@ -93,24 +109,49 @@ public class DemoQATest {
         WebElement rowCurrentAddress = driver.findElement(By.cssSelector("#currentAddress"));
         WebElement rowPermanentAddress = driver.findElement(By.cssSelector("#permanentAddress"));
 
-        Assert.assertEquals(rowName.getText(),"Name:Viktoriia Borozynets");
-        Assert.assertEquals(rowEmail.getText(),"Email:v.borozynets@gmail.com");
+        Assert.assertEquals(rowName.getText(), "Name:Viktoriia Borozynets");
+        Assert.assertEquals(rowEmail.getText(), "Email:v.borozynets@gmail.com");
         Assert.assertEquals(rowCurrentAddress, "Current Address :Lviv");
         Assert.assertEquals(rowPermanentAddress.getText(), "Permananet Address :Lviv");
 
+//        Map<String, String> map = new HashMap<>();
+//        map.put("Name","Viktoriia Borozynets");
+//        map.put("Email", "v.borozynets@gmail.com");
+//        map.put("Current Address", "Lviv");
+//        map.put("Permananet Address", "Lviv");
+
+        Map<String, String> mapNew = new HashMap<>();
+
         Map<String, String> map = new HashMap<>();
-        map.put("Name","Viktoriia Borozynets");
-        map.put("Email", "v.borozynets@gmail.com");
-        map.put("Current Address", "Lviv");
-        map.put("Permananet Address", "Lviv");
+
+        map.put("Name", "name");
+        map.put("Email", "email");
+        map.put("Current Address", "Current address");
+        map.put("Permananet Address", "Permanent Address");
+
+
+        WebElement enteredName = driver.findElement(By.id("name"));
+        String[] newName = enteredName.getText().split(":");
+        mapNew.put(newName[0], newName[1]);
+
+        WebElement enteredEmail = driver.findElement(By.id("email"));
+        String[] newEmail = enteredEmail.getText().split(":");
+        mapNew.put(newEmail[0], newEmail[1]);
+
+        WebElement enteredCurrentAddress = driver.findElement(By.cssSelector("#currentAddress.mb-1"));
+        String[] newCurrentAddress = enteredCurrentAddress.getText().split(":");
+        mapNew.put(newCurrentAddress[0].trim(), newCurrentAddress[1]);
+
+        WebElement enteredPermanentAddress= driver.findElement(By.cssSelector("#permanentAddress.mb-1"));
+        String[] newPermanentAddress = enteredPermanentAddress.getText().split(":");
+
+        mapNew.put(newPermanentAddress[0].trim(), newPermanentAddress[1]);
+
+        Assert.assertEquals(map, mapNew);
     }
 
-    @AfterTest
-    public void teardown() {
-        driver.quit();
-    }
+        @AfterTest
+        public void teardown() {
+            driver.quit();
+        }
 }
-
-
-
-
